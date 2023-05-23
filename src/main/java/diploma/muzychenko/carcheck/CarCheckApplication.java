40aeb215251d9193db8d55817e35aea9c69d1897
max <m.muz.al3105@gmail.com>
2023-05-23 23:26:16 +0300

    Initial commit

---
+++ src/main/java/diploma/muzychenko/carcheck/CarCheckApplication.java
@@ -0,0 +1,13 @@
+package diploma.muzychenko.carcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarCheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/diploma/muzychenko/carcheck/CarCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package diploma.muzychenko.carcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
